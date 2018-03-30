f225089900a9c54bb0479348c8871fcf6867066c
AJM249 <ajm249@pitt.edu>
2018-03-30 13:15:06 -0400

    Added support for file swapping.

--- xv6-public/proc.h
+++ xv6-public/proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  struct file *swapFile;       // Page/swap file.  Must initiate with createSwapFile.
 };
 
 // Process memory is laid out contiguously, low addresses first:
